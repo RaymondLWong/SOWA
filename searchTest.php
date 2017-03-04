cdd560bd4ff91e4037aaeff8b71f9e84dac63bf1
Raymond Wong <raymond.wong.1408@gmail.com>
2017-03-04 15:01:06 +0000

    create test page for search

---
+++ searchTest.php
@@ -0,0 +1,14 @@
+<?php
+if(!empty($_POST)){
+    $currentPage = $_SERVER['REQUEST_URI'];
+    $newPage = str_replace('searchTest.php', 'search.php?name=' . $_POST['search'], $currentPage);
+    header('Location: '. $newPage);
+//    http_redirect('/images/hut.png');
+}
+?>
+
+<form action="searchTest.php" method="post">
+    <label for="search">Search a property by its description.</label>
+    <input type="text" name="search" id="search" value="desc"/>
+    <input type="submit" name="submit" value="Search">
+</form>
