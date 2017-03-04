73536119570dc6d21393f1cd6a9b0af9c91c615a
Raymond Wong <raymond.wong.1408@gmail.com>
2017-03-04 18:40:26 +0000

    lv4 test

---
+++ portal.php
@@ -0,0 +1,13 @@
+<?php
+if(!empty($_POST)){
+    $currentPage = $_SERVER['REQUEST_URI'];
+    $newPage = str_replace('portal.php', 'aggregate.php?name=' . $_POST['search'], $currentPage);
+    header('Location: '. $newPage);
+}
+?>
+
+<form action="portal.php" method="post">
+    <label for="search">Search a property by its description.</label>
+    <input type="text" name="search" id="search" value="desc"/>
+    <input type="submit" name="submit" value="Search"/>
+</form>
