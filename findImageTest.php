99e387d27898e93b1b1eaca71d82bdfe619582ef
Raymond Wong <raymond.wong.1408@gmail.com>
2017-03-07 19:41:38 +0000

    test findImage

---
+++ findImageTest.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['submit'])){
+    $currentPage = $_SERVER['REQUEST_URI'];
+    $newPage = str_replace('findImageTest.php', 'findImage.php?picID=' . $_POST['search'], $currentPage);
+    header('Location: '. $newPage);
+}
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    <label for="search">Search a property picture by its.</label>
+    <input type="text" name="search" id="search" value="2"/>
+    <input type="submit" name="submit" value="Search">
+</form>
