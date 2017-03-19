5b7f41efcc2b61fb1f56554663e9a736de1d70f5
Raymond Wong <raymond.wong.1408@gmail.com>
2017-03-19 13:19:28 +0000

    add client-side XSL to Lv6, add Lv7

--- Level_6/Level_6.php
+++ Level_6/Level_6.php
@@ -4,7 +4,10 @@
     </head>
     <body>
         <div>
-            <a href="XSLT.php">PHP page of Level 5 that uses XSLT to parse the aggregated search results XML</a>
+            <a href="XSLT.php">PHP page based on Level 5 that uses XSLT (server-side in PHP) to parse the XML aggregated search results</a>
+        </div>
+        <div>
+            <a href="../Level_7/XHR.php">PHP page based on Level 5 that uses XSLT (client-side in JS) to parse the XML aggregated search results</a>
         </div>
         <div>
             <a href="../common/properties.xsl">XSL file used to parse XML</a>
---
+++ Level_7/Level_7.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Level 6</title>
+    </head>
+    <body>
+        <div>
+            <a href="XHR.php">PHP page based on Level 6 (XSLT) that uses XHR (in JS) to request the XML from the Level 4 aggregate web service</a>
+        </div>
+        <div>
+            <a href="../common/properties.xsl">XSL file used to parse XML</a>
+        </div>
+    </body>
+</html>
